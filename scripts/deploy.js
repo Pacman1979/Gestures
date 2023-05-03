2fc0dd03dbea8198d453d4c1ec8e3e972581bbcc
Phil Campbell <cryptotraderphil@protonmail.com>
2023-05-03 15:36:50 +1000

    WIP Metadata

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -22,8 +22,6 @@ async function main() {
   console.log(`NFT deployed to: ${nft.address}\n`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
